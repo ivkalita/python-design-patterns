d9c4441eb648088a1da14c24f436f1fb6a5658fa
Ivan Kalita <ivkalita@domclick.ru>
2018-03-11 09:39:44 +0300

    Remove unnecessary "pass" from abstract methods in ABC usage example.

--- abc_usage/runnable.py
+++ abc_usage/runnable.py
@@ -4,22 +4,13 @@ import abc
 class Runnable(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def run(self) -> None:
-        """
-        Make an instance run.
-        """
-        pass
+        """ Make an instance run. """
 
     @abc.abstractmethod
     def stop(self) -> None:
-        """
-        Stop the instance.
-        """
-        pass
+        """ Stop the instance. """
 
     @property
     @abc.abstractmethod
     def is_running(self) -> bool:
-        """
-        Returns true if instance is running.
-        """
-        pass
+        """ Returns true if instance is running. """
